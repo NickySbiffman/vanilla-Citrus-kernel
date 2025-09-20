16b0f095ceb9ec5ffcf96217fc5bfe933983a369
Nicky Sbiffman <nickysbiffman@gmail.com>
2025-09-20 22:07:05 +0300

    Project file structure: moved header files to include/citrus/ + added .gitignore

---
+++ include/citrus/tty.h
@@ -0,0 +1,8 @@
+#ifndef VGAD_H
+#define VGAD_H
+#include <types.h>
+
+void putchk(char c);
+void putsk(const char *str);
+void cleark();
+#endif
--- include/tty.h
+++
@@ -1,8 +0,0 @@
-#ifndef VGAD_H
-#define VGAD_H
-#include <types.h>
-
-void putchk(char c);
-void putsk(const char *str);
-void cleark();
-#endif
